80eff6b6309e7784416ebde3f459366765fbce83
CHINNAKRISHNA143 <chinnikrishnamekala1@gmail.com>
2026-04-03 21:07:50 +0530

    CRUD OperationsImpl

--- src/main/java/com/task/controller/TaskController.java
+++ src/main/java/com/task/controller/TaskController.java
@@ -1,6 +1,68 @@
 package com.task.controller;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.task.model.Task;
+import com.task.service.TaskService;
+
+@RestController
+@RequestMapping("/task")
 public class TaskController {
-
+	
+	@Autowired
+	private TaskService taskService;
+	
+	@PostMapping("/create")
+	public Task create(@RequestBody Task task) {
+		return taskService.createTask(task);
+	}
+	
+	@GetMapping("/getAllTasks")
+	public List<Task> allTasks(){
+		return taskService.getAllTasks();
+	}
+	
+	@GetMapping("/{id}")
+	public Task getById(@PathVariable Long id){
+		return taskService.getTaskById(id);
+	}
+	
+	@PutMapping("/update/{id}")
+	public Task updateingTask(@PathVariable Long id,@RequestBody Task task) {
+		return taskService.updateTask(id, task);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public void deleteingTask(@PathVariable Long id) {
+		taskService.deleteTask(id);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/main/java/com/task/service/TaskService.java
+++ src/main/java/com/task/service/TaskService.java
@@ -1,5 +1,8 @@
 package com.task.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +18,63 @@ public class TaskService {
 	public Task createTask(Task task){
 		return taskRepository.save(task);
 	}
-
+	
+	public List<Task> getAllTasks(){
+		return taskRepository.findAll();
+	}
+	
+	public Task getTaskById(Long id){
+		return  taskRepository.findById(id).orElse(null);
+		  /*Optional<Task> optionalTask = taskRepository.findById(id);
+		  
+		  if(optionalTask.isPresent()) {
+			  Task task = optionalTask.get();
+			  return task;
+		  }
+		  return null;*/
+	}
+	
+	public Task updateTask(Long id , Task updatedTask) {
+		
+		Optional<Task>  optionalTask = taskRepository.findById(id);
+		if(optionalTask.isPresent()) {
+			Task prevTask = optionalTask.get();
+			prevTask.setTitle(updatedTask.getTitle());
+			prevTask.setDescription(updatedTask.getDescription());
+			prevTask.setStatus(updatedTask.getStatus());
+			
+			return taskRepository.save(prevTask);
+		}
+		
+		return null;
+	}
+	
+	public void deleteTask(Long id) {
+		taskRepository.deleteById(id);
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
